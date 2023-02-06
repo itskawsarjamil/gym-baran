d5b3f9a1e8d52a720fd14e763a369352154b09f7
Kawsar Jamil <68264633+itskawsarjamil@users.noreply.github.com>
2023-02-06 17:33:57 +0600

    some litter warning removed

--- components/Header.js
+++ components/Header.js
@@ -30,11 +30,11 @@ const Header = () => {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             aria-hidden="true"
-                            class="w-5 h-5"
+                            className="w-5 h-5"
                         >
                             <line x1="3" y1="12" x2="21" y2="12"></line>
                             <line x1="3" y1="6" x2="21" y2="6"></line>
@@ -46,9 +46,9 @@ const Header = () => {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             aria-hidden="true"
                         >
                             <line x1="18" y1="6" x2="6" y2="18"></line>
